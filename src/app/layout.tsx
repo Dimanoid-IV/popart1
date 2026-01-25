dd1b16c8bf4e0f266059d59ce48b3081bc5880ae
Dmitri Ivkin <popartee@gmail.com>
2026-01-25 13:16:59 +0200

    Add Google Analytics tag (G-H52TSV34MX) to track website visitors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -48,6 +48,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-H52TSV34MX"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-H52TSV34MX');
+            `,
+          }}
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
